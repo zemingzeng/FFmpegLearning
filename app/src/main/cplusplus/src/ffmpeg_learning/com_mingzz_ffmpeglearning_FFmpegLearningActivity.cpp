026d59ba42b035750bf4914ac32df4dc6cc0eb19
Mingzz_ <zemingzeng@126.com>
2023-11-22 15:49:41 +0800

    config ffmpeg libs and print some version info of avcodec...

--- app/src/main/cplusplus/src/ffmpeg_learning/com_mingzz_ffmpeglearning_FFmpegLearningActivity.cpp
+++ app/src/main/cplusplus/src/ffmpeg_learning/com_mingzz_ffmpeglearning_FFmpegLearningActivity.cpp
@@ -3,7 +3,26 @@
 #include "FFmpegLearning.h"
 #include "Log.h"
 
-jstring FFmpegLearningActivity_stringFromJNI(JNIEnv *env,jobject jOject){
-    std::string info("FFmpegLearningActivity show!!!!!");
-    return  env->NewStringUTF(info.c_str());
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavutil/ffversion.h"
+}
+
+jstring FFmpegLearningActivity_stringFromJNI(JNIEnv *env, jobject jOject) {
+    std::string info("FFmpegLearningActivity : \n");
+    unsigned int avcodecVersionNumber = avcodec_version();
+    LOGD("ffmpeg avcodecVersion-->%d(Version=[major:minor:micro]-->%d.%d.%d) ffmpegVersion-->%s ",
+         avcodecVersionNumber,
+         (avcodecVersionNumber >> 16) & 0xff,
+         (avcodecVersionNumber >> 8) & 0xff,
+         (avcodecVersionNumber) & 0xff,
+         FFMPEG_VERSION);
+    info += "ffmpeg version->" + std::string(FFMPEG_VERSION) + "\n" +
+            "ffmpeg avcodecVersion->" + std::to_string(avcodecVersionNumber) + "\n" +
+            "(Version=[major:minor:micro]-->" +
+            std::to_string((avcodecVersionNumber >> 16) & 0xff) + "." +
+            std::to_string((avcodecVersionNumber >> 8) & 0xff) + "." +
+            std::to_string((avcodecVersionNumber) & 0xff) + "." +
+            ")";
+    return env->NewStringUTF(info.c_str());
 }
